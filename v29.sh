a49a1dad114b18b877305e5bfedca3e6263c7567
Nendrik <118098449+nendrik@users.noreply.github.com>
2025-01-12 18:09:19 +0700

    Update v29.sh

--- v29.sh
+++ v29.sh
@@ -12,8 +12,8 @@ sudo apt upgrade
 sudo apt-get install figlet
 figlet -f /usr/share/figlet/starwars.flf
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.29.0/executor-linux-v0.29.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.29.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.32.0/executor-linux-v0.32.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.32.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
