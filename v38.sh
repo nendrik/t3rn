3bd44c1a1005c8dce6dfc59849da363a88f2e591
Nendrik <118098449+nendrik@users.noreply.github.com>
2025-01-21 12:02:18 +0700

    Update and rename v31.sh to v38.sh

--- v38.sh
+++ v38.sh
@@ -12,8 +12,8 @@ sudo apt upgrade
 sudo apt-get install figlet
 figlet -f /usr/share/figlet/starwars.flf
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.31.0/executor-linux-v0.31.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.31.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.38.0/executor-linux-v0.38.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.38.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
@@ -54,6 +54,7 @@ export RPC_ENDPOINTS_ARBT="https://arb-sepolia.g.alchemy.com/v2/$KEYALCHEMY"
 export RPC_ENDPOINTS_BSSP="https://base-sepolia.g.alchemy.com/v2/$KEYALCHEMY"
 export RPC_ENDPOINTS_BLSS="https://blast-sepolia.g.alchemy.com/v2/$KEYALCHEMY"
 export RPC_ENDPOINTS_OPSP="https://opt-sepolia.g.alchemy.com/v2/$KEYALCHEMY"
+export RPC_ENDPOINTS_L1RN="https://brn.rpc.caldera.xyz/http"
 
 
 sleep 2
