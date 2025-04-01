7fac72b88ec9cbaacbdeb74e326dae4b61ccd745
Nendrik <118098449+nendrik@users.noreply.github.com>
2025-04-01 18:05:30 +0700

    Update arka.sh

--- arka.sh
+++ arka.sh
@@ -38,14 +38,15 @@ echo -e "\nPrivate key has been set."
 echo
 
 export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,unichain-sepolia,l2rn'
-
 export RPC_ENDPOINTS='{
     "l2rn": ["https://b2n.rpc.caldera.xyz/http"],
     "arbt": ["https://arb-sepolia.g.alchemy.com/v2/NAUs2i0yEWAWtCryCshtI4AYNoP-QDKz", "https://arbitrum-sepolia.drpc.org"],
     "bast": ["https://base-sepolia.g.alchemy.com/v2/NAUs2i0yEWAWtCryCshtI4AYNoP-QDKz", "https://base-sepolia-rpc.publicnode.com"],
+    "blst": ["https://blast-sepolia.g.alchemy.com/v2/NAUs2i0yEWAWtCryCshtI4AYNoP-QDKz"],
     "opst": ["https://opt-sepolia.g.alchemy.com/v2/NAUs2i0yEWAWtCryCshtI4AYNoP-QDKz", "https://sepolia.optimism.io"],
     "unit": ["https://unichain-sepolia.g.alchemy.com/v2/NAUs2i0yEWAWtCryCshtI4AYNoP-QDKz", "https://unichain-sepolia.drpc.org"]
 }'
+
 
 sleep 2
 echo "Starting the Executor..."
